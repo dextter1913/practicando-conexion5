d2f7331a18f1de05d86675f9839bc08a15f432a4
dextter1913 <Clau32usa@gmai.com>
2021-02-24 21:07:05 -0500

    Actualizacion

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>practicando conexion 5</title>
+</head>
+<body>
+<center>
+<h1>Practicando Conexion 5</h1>
+<form action="index.php" method="post">
+<label for="usuario">Usuario:</label>
+<input type="text" name="usuario" id="usuario">
+<label for="contraseña">Contraseña:</label>
+<input type="password" name="contraseña" id="contraseña">
+<label for="contraseña2">Confirmar Contraseña:</label>
+<input type="password" name="contraseña2" id="contraseña2">
+<label for="id">Doc Identidad</label>
+<input type="text" name="id" id="id">
+<label for="nombre">Nombre Estudiante:</label>
+<input type="text" name="nombre" id="nombre">
+<label for="apellido">Primer Apellido:</label>
+<input type="text" name="apellido" id="apellido">
+<label for="apellido2">Segundo Apellido:</label>
+<input type="text" name="apellido2" id="apelido2">
+<label for="telefono">Telefono:</label>
+<input type="text" name="telefono" id="telefono">
+<input type="submit" value="Registrar">
+
+</form>
+</center>
+    
+</body>
+</html>
